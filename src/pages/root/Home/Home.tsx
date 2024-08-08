0596a87a8abc95be208761b7e43140ceae38b427
sahibibadov <bdvshb@gmail.com>
2024-08-08 10:34:31 +0400

    fixed:home page image,loading component

--- src/pages/root/Home/Home.tsx
+++ src/pages/root/Home/Home.tsx
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <AnimatedLayout>
       {/* hero */}
-      <section className="h-[100vh] md:h-[calc(100vh-64px)] bg ">
+      <section className="h-[calc(100vh-64px)] bg ">
         <div className="text">
           <AnimatedReveal delay={0.45} blur="6px">
             <h1 className="font-serif uppercase text-4xl md:text-5xl text-center  max-w-6xl mx-auto">
@@ -17,7 +17,7 @@ const Home = () => {
             <p className="text-center">İncəsənətə yeni baxış</p>
           </AnimatedReveal>
         </div>
-        <div className="h-[100vh] md:h-[calc(100vh-64px)] bg-blur"></div>
+        <div className="h-[calc(100vh-64px)] bg-blur"></div>
       </section>
 
       {/* products */}
